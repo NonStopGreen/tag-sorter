8e3ff867c99d80bffcb4df48030b8436b8c4c075
anonymous675 <30692665+anonymous675@users.noreply.github.com>
2020-07-25 19:43:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
@@ -14,96 +13,97 @@
   }
 }(typeof self !== 'undefined' ? self : this, function() {
     'use strict';
-    return function searchDocumentsByTags({documents = [], tags = [], ignoredTags = [], max = Infinity, sort = true})  {
-      function sortDocumentsByTagsLength({object1, object2, tags}) {
+
+    class Tag {
+      constructor({ documents = [], tags = [], ignoredTags = []}) {
+        if (!Array.isArray(documents) || documents.length == 0 || !documents.every(d => (typeof d === "object")))
+          throw new TypeError('Parameter documents is not an array filled with objects');
+
+        if (!Array.isArray(tags) || tags.length == 0 || !tags.every(t => (typeof t === "string")))
+          throw new TypeError("Parameter tags is not an array filled with strings");
+
+        this._documents = documents;
+        this.documents = documents;
+        this.tags = tags;
+        this.ignoredTags = ignoredTags;
+      }
+
+      find() {
+        var i;
+
+        this.documents = this._documents.filter(doc => {
+          for (i = 0; i < this.ignoredTags.length; i++) {
+            if (doc.tags.includes(this.ignoredTags[i])) return;
+          }
+
+          for (i = 0; i < this.tags.length; i++) {
+            if (doc.tags.includes(this.tags[i]))
+              return doc
+          }
+        });
+
+        return this;
+      }
+
+      limit(number = Infinity) {
+        if (isNaN(number) || number < 0) throw new TypeError("Parameter number in limit() is not a positive number");
+
+        if (this.documents.length >= number) this.documents = this.documents.slice(0, number);
+        else throw new RangeError("Parameter number in limit() is bigger than documents length");
+
+        return this;
+      }
+
+      sort(sortMethod = -1) {
+        var self = this;
+        var sortMethod = sortMethod;
+
+        this.documents = this.documents.sort((object1, object2) => {
           var object1TotalTags = 0;
           var object2TotalTags = 0;
           var i;
 
-          for(i = 0; i < tags.length; i++) {
-            if (object1.tags.includes(tags[i])) object1TotalTags += 1;
-            if (object2.tags.includes(tags[i])) object2TotalTags += 1;
+          for(i = 0; i < self.tags.length; i++) {
+            if (object1.tags.includes(self.tags[i])) object1TotalTags += 1;
+            if (object2.tags.includes(self.tags[i])) object2TotalTags += 1;
           }
 
-          if (object1TotalTags < object2TotalTags) return 1;
-          else if (object1TotalTags > object2TotalTags) return -1;
+          if (sortMethod === "descending" || sortMethod === -1) {
+
+            if (object1TotalTags < object2TotalTags) return 1;
+            else if (object1TotalTags > object2TotalTags) return -1;
+
+          } else if (sortMethod === "ascending" || sortMethod === 1) {
+
+            if (object1TotalTags > object2TotalTags) return 1;
+            else if (object1TotalTags < object2TotalTags) return -1;
+
+          } else {
+            throw new TypeError('Parameter for sort() should be: "descending" or "ascending" or -1 or 1');
+          }
           return 0;
-      }
-
-      function filterOutDocumentsByIgnoredTags({object, index, max, ignoredTags}) {
-        var i;
-        for (i = 0; i < ignoredTags.length; i++) {
-          if (object.tags.includes(ignoredTags[i]))
-            return true;
-        }
-        return false;
-      }
-
-      function filterDocumentsByFollowedTags({object, index, max, tags}) {
-        var i;
-        for (i = 0; i < tags.length; i++) {
-          if (object.tags.includes(tags[i]))
-            return true;
-        }
-        return false;
-      }
-
-      function filterDocumentsByTags({object, index, max, tags, ignoredTags}) {
-        var found = false, targetMap = {}, i, j, cur;
-
-        // Check for ignored tags in object.tags
-        found = filterOutDocumentsByIgnoredTags({
-          object:object,
-          index:index,
-          max:max,
-          ignoredTags:ignoredTags
-        });
-
-        if (found) return;
-
-        // Check for followed tags in object.tags
-        found = filterDocumentsByFollowedTags({
-          object:object,
-          index:index,
-          max:max,
-          tags:tags
-        });
-
-        if (found) return object && index < max;
-      }
-
-      if (!Array.isArray(documents) || documents.length == 0 || !documents.every(d => (typeof d === "object")))
-        throw new TypeError('Parameter documents is not an array filled with objects')
-
-      if (!Array.isArray(tags) || tags.length == 0 || !tags.every(t => (typeof t === "string")))
-        throw new TypeError("Parameter tags is not an array filled with strings");
-
-      if (max && isNaN(max))
-        throw new TypeError("Parameter max is not a number");
-
-      if (sort !== true && sort !== false)
-        throw new TypeError("Parameter sort is not a boolean");
-
-      var filteredDocuments = documents.filter((documents, index) => {
-        return filterDocumentsByTags({
-          object:documents,
-          index:index,
-          max:max,
-          tags:tags,
-          ignoredTags:ignoredTags
         })
-      })
 
-      if (sort) {
-        filteredDocuments = filteredDocuments.sort((object1, object2) => {
-          return sortDocumentsByTagsLength({
-            object1:object1,
-            object2:object2,
-            tags:tags
-          })
-        })
+        return this;
       }
 
-      return filteredDocuments;
+      exec(fn) {
+        var allTags = this.documents.flatMap(d => d.tags);
+
+        var result = {};
+
+        result.documents = this.documents;
+
+        result.tagsCounted = allTags.reduce((prev, cur) => {
+          prev[cur] = (prev[cur] || 0) + 1;
+          return prev;
+        }, {});
+
+        result.uniqueTags = [...new Set(allTags)];
+
+        return (typeof(result) === "function") ? fn([], result) : result;
+      }
     }
+
+    return Tag;
 }));
